4c5366a7cd1dbfafa9887180b1eb667a4265cbd7
Younah22222 <youna051231@gmail.com>
2025-07-21 13:48:43 +0900

    Title: 높은 수에서 낮은 수까지, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250721/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 250721/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -2,9 +2,9 @@ arr=input().split()
 a=int(arr[0])
 b=int(arr[1])
 
-if a>b:
+if a>=b:
     for i in range (a,b-1,-1):
         prnt(i,end=' ')
-elif b>a:
+elif b>=a:
     for i in range (b,a-1,-1):
         print(i,end=' ')
